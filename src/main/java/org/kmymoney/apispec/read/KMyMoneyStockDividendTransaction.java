c4d8b2165473de46d8a5255ca8786bb8dce77c49
James Rossini <james.rossini@posteo.de>
2025-12-12 10:07:14 +0100

    KMyMoneyStockDividendTransaction(Impl): New (incl. test data / cases)

---
+++ src/main/java/org/kmymoney/apispec/read/KMyMoneyStockDividendTransaction.java
@@ -0,0 +1,22 @@
+package org.kmymoney.apispec.read;
+
+import java.util.List;
+
+import org.kmymoney.api.read.KMyMoneyTransaction;
+import org.kmymoney.api.read.KMyMoneyTransactionSplit;
+
+import xyz.schnorxoborx.base.beanbase.TransactionSplitNotFoundException;
+
+public interface KMyMoneyStockDividendTransaction extends KMyMoneyTransaction,
+												     KMyMoneySpecialTransaction
+{
+
+    public KMyMoneyTransactionSplit       getStockAccountSplit()  throws TransactionSplitNotFoundException;
+    
+    public KMyMoneyTransactionSplit       getIncomeAccountSplit()  throws TransactionSplitNotFoundException;
+    
+    public List<KMyMoneyTransactionSplit> getExpensesSplits()  throws TransactionSplitNotFoundException;
+    
+    public KMyMoneyTransactionSplit       getOffsettingAccountSplit()  throws TransactionSplitNotFoundException;
+    
+}
